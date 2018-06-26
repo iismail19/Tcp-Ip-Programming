55a36482933ddb3358c0b96d18661aca4c8077b8
iismail1997 <2015.ismail@gmail.com>
2018-06-26 01:15:56 -0500

    Created first server and connected to it using telnet

--- SocketServerStarter/SocketServerStarter/Program.cs
+++ SocketServerStarter/SocketServerStarter/Program.cs
@@ -12,6 +12,21 @@ namespace SocketServerStarter
     {
         static void Main(string[] args)
         {
+
+            // takes 3 Paramaters, Param 1: means we are useing IPv4, 2: using streaming, 3: TCP protocol
+            Socket listnerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = IPAddress.Any; // this means it will listen to incoming connections on any availble IP-address
+
+            IPEndPoint endpoint = new IPEndPoint(ipAddress, 23000); // IPAddress and port number = 23000
+
+            // bind socket to endpoint
+            listnerSocket.Bind(endpoint);
+            // will now listen, the number supplied will tell it how many clients can wait for a connection at
+            // any time while the system is busy with other connections
+            listnerSocket.Listen(5);
+
+            // call accept methond on listener socket
+            listnerSocket.Accept();
         }
     }
 }
