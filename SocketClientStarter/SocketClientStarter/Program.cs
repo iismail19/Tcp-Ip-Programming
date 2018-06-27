c354d98366b2fb901256d6015b3fc12da41f20e3
iismail1997 <2015.ismail@gmail.com>
2018-06-26 22:26:24 -0500

    New project SocketClientStarter

---
+++ SocketClientStarter/SocketClientStarter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClientStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = null;
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipaddr = null;
+            try
+            {
+
+            }
+            catch(Exception excp)
+            {
+                Console.WriteLine(excp.ToString());
+            }
+        }
+    }
+}
